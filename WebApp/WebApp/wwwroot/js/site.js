017713f92cff2fb2938a4bf0bdc43ad88edf6a88
Andreas Ingeholm <andreas.ingeholm@gmail.com>
2023-05-23 08:51:05 +0200

    Fixed js validation.

--- WebApp/WebApp/wwwroot/js/site.js
+++ WebApp/WebApp/wwwroot/js/site.js
@@ -19,3 +19,46 @@
 }
 
 toggleMenu('[data-option="toggle"]');
+
+const validateText = (event) => {
+
+    if (event.target.value.length >= 2)
+        document.querySelector(`[data-valmsg-for="${event.target.id}"]`).innerHTML = "";
+    else
+        document.querySelector(`[data-valmsg-for="${event.target.id}"]`).innerHTML = "Must be at least 1 character long.";
+
+};
+
+const validateEmail = (event) => {
+
+    const regex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+
+    if (regex.test(event.target.value))
+        document.querySelector(`[data-valmsg-for="${event.target.id}"]`).innerHTML = "";
+    else
+        document.querySelector(`[data-valmsg-for="${event.target.id}"]`).innerHTML = "Must be valid email address.";
+
+};
+
+
+const validatePassword = (event) => {
+
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+    if (regex.test(event.target.value))
+        document.querySelector(`[data-valmsg-for="${event.target.id}"]`).innerHTML = "";
+    else
+        document.querySelector(`[data-valmsg-for="${event.target.id}"]`).innerHTML = "Password must be at least 8 characters long, and must contain:\n\n* 1 lowercase character\n* 1 uppercase character\n* 1 number character\n* 1 special character";
+
+};
+
+const validatePhoneNumber = (event) => {
+
+    const regex = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/;
+
+    if (event.target.value == "" || regex.test(event.target.value))
+        document.querySelector(`[data-valmsg-for="${event.target.id}"]`).innerHTML = "";
+    else
+        document.querySelector(`[data-valmsg-for="${event.target.id}"]`).innerHTML = "Must be a valid phone number";
+
+};
